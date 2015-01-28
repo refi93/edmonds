cbb14ff528d6bd8dc3d811b1246987763ec7b82c
raf <rafael.korbas@gmail.com>
2015-01-28 23:55:17 +0100

    odstranena zbytocna matica a funkcia

--- src/edmonds/Graph.java
+++ src/edmonds/Graph.java
@@ -16,15 +16,14 @@ import java.util.HashSet;
 public class Graph {
     int vertexCount;
     
-    // matica susednosti, kde si pamatame cen
-    int[][] incidenceMatrix, fullfillMatrix;
+    // matica susednosti, kde si pamatame ceny
+    int[][] incidenceMatrix;
     
     ArrayList<Vertex> vertices; // mnozina vrcholov - k vrcholu si pamatame bubliny, kam patri
     
     public Graph(int vertexCount) {
         this.vertexCount = vertexCount;
         incidenceMatrix = new int[vertexCount][vertexCount];
-        fullfillMatrix = new int[vertexCount][vertexCount];
         vertices = new ArrayList<Vertex>();
         
         for(int i = 0; i < vertexCount; i++){
@@ -37,8 +36,7 @@ public class Graph {
         // nastavime ceny hran na nekonecno
         for (int i = 0; i < vertexCount; i++){
             for (int j = 0; j < vertexCount; j++){
-                incidenceMatrix[i][j] = 47000;
-                fullfillMatrix[i][j] = 0;
+                incidenceMatrix[i][j] = Variables.INFTY;
             }
         }
     }
@@ -46,12 +44,6 @@ public class Graph {
     public void addEdge(int v1, int v2, int price) {
         incidenceMatrix[v1][v2] = price;
         incidenceMatrix[v2][v1] = price;
-    }
-    
-    // naplnanie hran
-    public void fillEdge(int v1, int v2, int val) {
-        fullfillMatrix[v1][v2] += val;
-        fullfillMatrix[v2][v1] += val;
     }
     
     public int getR(){
